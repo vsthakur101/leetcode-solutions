2787681fc6688fa028ac775888c3baece1b3cc7e
Vidhya Sagar Thakur <60700534+vsthakur101@users.noreply.github.com>
2025-05-01 13:13:47 +0530

    Time: 0 ms (100%), Space: 53.7 MB (79.79%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    if (!strs.length) return "";
+
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (!strs[i].startsWith(prefix)) {
+            prefix = prefix.slice(0, -1);
+            if (!prefix) return "";
+        }
+    }
+
+    return prefix;
+};
