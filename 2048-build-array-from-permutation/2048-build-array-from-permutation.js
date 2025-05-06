699dcce95ffffd513e47b5183f90afc31820fa36
Vidhya Sagar Thakur <60700534+vsthakur101@users.noreply.github.com>
2025-05-06 17:43:04 +0530

    Time: 1 ms (74.9%), Space: 59.2 MB (51.02%) - LeetHub

---
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    let ans = new Array(nums.length);
+    for(let i =0; i < nums.length; i++){
+        ans[i] = nums[nums[i]];
+    }
+    return ans
+
+};
