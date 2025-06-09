9601af96a671f06d466e6fc1e40ee13c30ab63bf
Vidhya Sagar Thakur <60700534+vsthakur101@users.noreply.github.com>
2025-06-09 15:42:57 +0530

    Time: 15 ms (56.54%), Space: 56 MB (89.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -7,12 +7,14 @@ var isAnagram = function (s, t) {
     if (s.length !== t.length)
         return false;
     let map = new Map();
-    for (let char of s) {
-        map.set(char, (map.get(char) || 0) + 1)
+    for (let char= 0; char < s.length; char++) {
+        map.set(s[char], (map.get(s[char]) || 0) + 1);
+        map.set(t[char], (map.get(t[char]) || 0) - 1)
     }
-    for (let char of t) {
-        if (!map.has(char) || map.get(char) === 0) return false;
-        map.set(char, map.get(char) - 1)
+    for (let i=0; i < s.length; i++) {
+        if (map.get(s[i]) > 0 ) {
+            return false
+        }
     }
     return true
 };
