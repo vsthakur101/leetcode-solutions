2c5fedb96e25474b5a8883d9fda9ebebaaab5862
Vidhya Sagar Thakur <60700534+vsthakur101@users.noreply.github.com>
2025-07-04 17:54:36 +0530

    Time: 1 ms (89.13%), Space: 60.5 MB (45.03%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+    function check(node) {
+        if (!node) return 0;
+
+        let left = check(node.left);
+        if (left === -1) return -1;
+
+        let right = check(node.right);
+        if (right === -1) return -1;
+
+        if (Math.abs(left - right) > 1) return -1;
+
+        return 1 + Math.max(left, right);
+    }
+
+    return check(root) !== -1;
+};
