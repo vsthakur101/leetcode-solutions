b26041ea4a4ca0f4beb8a7fd2aa663ddfe05328c
Vidhya Sagar Thakur <60700534+vsthakur101@users.noreply.github.com>
2025-08-04 06:21:10 -0700

    Time: 6 ms (67.96%), Space: 71.1 MB (48.29%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -5,16 +5,16 @@
 var productExceptSelf = function(nums) {
     const output = Array(nums.length).fill(1);
 
-    let left = 1;
+    let main = 1;
     for (let i = 0; i < nums.length; i++) {
-        output[i] *= left;
-        left *= nums[i];
+        output[i] *= main;
+        main *= nums[i];
     }
 
-    let right = 1;
+    main = 1;
     for (let i = nums.length - 1; i >= 0; i--) {
-        output[i] *= right;
-        right *= nums[i];
+        output[i] *= main;
+        main *= nums[i];
     }
 
     return output;    
