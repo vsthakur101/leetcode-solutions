45398c713e43a3376928288969e2021ee1361649
Vidhya Sagar Thakur <60700534+vsthakur101@users.noreply.github.com>
2025-08-10 09:21:39 -0700

    Time: 1 ms (97.16%), Space: 61.1 MB (95.74%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+  const n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    while (
+      nums[i] >= 1 &&
+      nums[i] <= n &&
+      nums[nums[i] - 1] !== nums[i]
+    ) {
+      const targetIdx = nums[i] - 1;
+      const tmp = nums[i];
+      nums[i] = nums[targetIdx];
+      nums[targetIdx] = tmp;
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) return i + 1;
+  }
+  return n + 1;
+};
