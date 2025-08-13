da12586c4e97696e44fbcd041d7124344390a814
Vidhya Sagar Thakur <60700534+vsthakur101@users.noreply.github.com>
2025-08-13 08:23:41 -0700

    Time: 0 ms (100%), Space: 55.7 MB (66.4%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,10 +3,8 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let n = nums.length;
-    if(n < 2) return nums[0]
     let result = nums[0]
-    for(let i = 1; i < n; i++){
+    for(let i = 1; i < nums.length; i++){
         result ^= nums[i]
     }
     return result;
