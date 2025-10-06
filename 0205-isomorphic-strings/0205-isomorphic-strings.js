178bb930de1e4aa19c0c7fed41907c8da0e9fee9
Vidhya Sagar Thakur <60700534+vsthakur101@users.noreply.github.com>
2025-10-06 07:08:54 -0400

    Time: 4 ms (89.74%), Space: 54.5 MB (82.08%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -3,26 +3,23 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
-    if(s.length !== t.length) return false;
-    const mapST = new Map(); // s -> t
-    const mapTS = new Map(); // t -> s
+var isIsomorphic = function (s, t) {
+    if (s.length !== t.length) return false;
+
+    const map = new Map();
+    const used = new Set();
 
     for (let i = 0; i < s.length; i++) {
-        const cs = s[i], ct = t[i];
+        const a = s[i], b = t[i];
 
-        if (mapST.has(cs)) {
-            if (mapST.get(cs) !== ct) return false;
-        } else {
-            mapST.set(cs, ct);
-        }
-
-        if (mapTS.has(ct)) {
-            if (mapTS.get(ct) !== cs) return false;
-        } else {
-            mapTS.set(ct, cs);
+        if (!map.has(a)) {
+            if (used.has(b)) return false;
+            map.set(a, b);
+            used.add(b);
+        } else if (map.get(a) !== b) {
+            return false;
         }
     }
-    return true;
 
+    return true;
 };
